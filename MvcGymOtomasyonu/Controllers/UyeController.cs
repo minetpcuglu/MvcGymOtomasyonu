382fd54168359a1df33fdb31cc3d96531b1daf99
minetpcuglu <minetopcuoglu6@gmail.com>
2021-04-23 17:13:23 +0300

    Add project files.

---
+++ MvcGymOtomasyonu/Controllers/UyeController.cs
@@ -0,0 +1,24 @@
+﻿using MvcGymOtomasyonu.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcGymOtomasyonu.Controllers
+{
+    public class UyeController : Controller
+    {
+        // GET: Uye
+        SporDbEntities db = new SporDbEntities();
+        public ActionResult Index()
+        {
+            // GET: Musteri
+          
+           
+                var degerler = db.TblUye.ToList();
+                return View(degerler);
+         
+        }
+    }
+}
